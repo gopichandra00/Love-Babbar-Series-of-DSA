eaa9fee7478da3ffe3b2638e586a3a1cc2c2c2fc
Gopi Chandra Azad <gopiazad12338@gmail.com>
2023-03-16 14:09:20 +0530

    This is the third program of lovebabbar series

--- Kth_max_min.py
+++ Kth_max_min.py
@@ -1,9 +1,51 @@
 class Solution:
-    def KthMM(self, A, start, end):
-        if start == end:
-            return A
+    def merge_sort(self, arr):
+        if len(arr) <= 1:
+            return arr
+
+        mid = len(arr) // 2
+        left = self.merge_sort(arr[:mid])
+        right = self.merge_sort(arr[mid:])
+
+        i, j = 0, 0
+        merged = []
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                merged.append(left[i])
+                i += 1
+            else:
+                merged.append(right[j])
+                j += 1
+        merged += left[i:]
+        merged += right[j:]
+        return merged
+
+    def kth_max_min(self, arr, k):
+        arr = self.merge_sort(arr)
+        if k <= len(arr):
+            kth_min = arr[k-1]
+            kth_max = arr[-k]
+            return kth_min, kth_max
         else:
-            mid = (start+end)//2
-            max_1, min_1 = self.KthMM(A, start, mid)
-            max_2, min_2 = self.KthMM(A,mid+1,end)
-            A[start], A[end] = A[end], A[end]
+            return None
+
+
+arr = [3, 32, 14, 34, 31, 44, 11, 41, 4]
+obj = Solution()
+array_ = obj.merge_sort(arr)
+print(array_)
+k = int(input("Enter the kth value in range of array:\t"))
+result = obj.kth_max_min(arr, k)
+if result:
+    kth_min, kth_max = result
+    print("Kth minimum element: ", kth_min)
+    print("Kth maximum element: ", kth_max)
+else:
+    print("Kth number you entered is out of range ")
+
+
+
+
+
+
+
